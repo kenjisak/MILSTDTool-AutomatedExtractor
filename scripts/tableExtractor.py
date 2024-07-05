67f8cbc09c0ef754492b13a66561b69a4c925642
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2024-07-05 16:01:10 -0400

    added page extraction to work for figures data as well

--- scripts/tableExtractor.py
+++ scripts/tableExtractor.py
@@ -5,38 +5,44 @@ import camelot
 
 milstdpdf_file_path = '../resources/MIL-STD-1472H.pdf'
 tabledata_file_path = '../resources/tableData.txt'
+figuredata_file_path = '../resources/figureData.txt'
 
 # Function to extract integers from each line in a file
 def extract_pageNumbers_from_file(txt_file_path):
-    pageNumberList = SortedSet()
+    # pageNumberList = list()
+    pageNumberSet = SortedSet()
 
     with open(txt_file_path, 'r') as file:
         for line in file:
-            # Find all integers in the line using regex
-            pageNumber = re.search(r'\d+', line)
-            pageNumberList.add(int(pageNumber.group()))
-            # print(f"Page Number in line: {pageNumber}")
-            # TODO for figures or in general, only select the last integer found in the line
-    return pageNumberList
-# TODO add figure/image extraction plus table extraction for that same page to be paired with
+            pageNumbers = re.findall(r'\d+', line)
+            pageNumberSet.add(int(pageNumbers[-1]))
+            # pageNumberList.append(int(pageNumbers[-1]))
+    # return pageNumberSet, pageNumberList
+    return pageNumberSet
 
+
+# TODO add figure/image extraction plus table extraction for that same page to be paired with
 # TODO add api update requests to google sheets doc after each table
 # TODO use a sheet storage for persistence in pages alreayd processed for fast processing
 
 
-
 tables_page_numbers = extract_pageNumbers_from_file(tabledata_file_path)
-# print(tables_page_numbers)
+# print(len(tables_page_numbers[1]))
+# print(tables_page_numbers[0])
+
+figures_page_numbers = extract_pageNumbers_from_file(figuredata_file_path)
+# print(len(figures_page_numbers[1]))
+# print(figures_page_numbers[0])
 page_offset = 17 # page 1 starts after page 17, page offset + extracted page = correct page number
 
 for i in range(len(tables_page_numbers)):
     corrected_page_number = tables_page_numbers[i] + page_offset
-    # currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(corrected_page_number))
-    while True:
-        currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(47 + page_offset))
+    currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(corrected_page_number))
+    # while True:
+    #     currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(47 + page_offset))
 
-        for j in range(len(currTables)):
-            # print(currTable[i].df)
-            print("Page Number: " + str(tables_page_numbers[i]) + str(currTables[j].parsing_report))
-            if currTables[j].parsing_report['accuracy'] >= 97:
-                break
+    for j in range(len(currTables)):
+        # print(currTable[i].df)
+        print("Page Number: " + str(tables_page_numbers[i]) + str(currTables[j].parsing_report))
+        if currTables[j].parsing_report['accuracy'] >= 97:
+            break
