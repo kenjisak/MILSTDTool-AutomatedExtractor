36878f0982b4a0dc29922e578c2be08e7bab6555
Kenji Isak Laguan <120000674+kenjisak@users.noreply.github.com>
2024-07-08 14:08:21 -0400

    refactored code into respective methods and main function for better structure

--- scripts/tableExtractor.py
+++ scripts/tableExtractor.py
@@ -87,25 +87,34 @@ def upload_csv_file(csv_file_path):
 
 # upload_csv_file("../resources/tablesCSV/" + "TABLE I. Mechanical control criteria.csv")
 
-tables_page_numbers = extract_pageNumbers_from_file(tabledata_file_path)
-figures_page_numbers = extract_pageNumbers_from_file(figuredata_file_path)
+def extract_tables():
+    tables_page_numbers = extract_pageNumbers_from_file(tabledata_file_path)
+    figures_page_numbers = extract_pageNumbers_from_file(figuredata_file_path)
 
-tables_page_titles = extract_titles_from_file(tabledata_file_path)
-figures_page_titles = extract_titles_from_file(figuredata_file_path)
+    tables_page_titles = extract_titles_from_file(tabledata_file_path)
+    figures_page_titles = extract_titles_from_file(figuredata_file_path)
 
-page_offset = 17 # page 1 starts after page 17, page offset + extracted page = correct page number
-title_index_counter = 0
+    page_offset = 17 # page 1 starts after page 17, page offset + extracted page = correct page number
+    title_index_counter = 0
 
-for i in range(len(tables_page_numbers)):
-    corrected_page_number = tables_page_numbers[i] + page_offset
-    currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(corrected_page_number))
-    # while True:
-    #     currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(47 + page_offset))
+    for i in range(len(tables_page_numbers)):
+        corrected_page_number = tables_page_numbers[i] + page_offset
+        currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(corrected_page_number))
+        # while True:
+        #     currTables = camelot.read_pdf(milstdpdf_file_path,pages = str(47 + page_offset))
 
-    for j in range(len(currTables)):
-        # print(currTable[i].df)
-        print("Page Number: " + str(tables_page_numbers[i]) + str(currTables[j].parsing_report))
-        currTables[j].to_csv("../resources/tablesCSV/" + tables_page_titles[title_index_counter] + '.csv')
-        title_index_counter += 1
-        # if currTables[j].parsing_report['accuracy'] >= 97:
-        #     break
+        for j in range(len(currTables)):
+            # print(currTable[i].df)
+            print("Page Number: " + str(tables_page_numbers[i]) + str(currTables[j].parsing_report))
+            currTables[j].to_csv("../resources/tablesCSV/" + tables_page_titles[title_index_counter] + '.csv')
+            title_index_counter += 1
+            # if currTables[j].parsing_report['accuracy'] >= 97:
+            #     break
+
+def main():
+    # extract_tables()
+    # upload_csv_file("../resources/tablesCSV/" + "TABLE I. Mechanical control criteria.csv")
+    return
+
+if __name__ == "__main__":
+    main()
